0efe3adc39c13a20e5a05c58885366bbd386dbf4
wars_skynox12 <pasnon.lucas@gmail.com>
2023-09-02 23:52:32 +0200

    six

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { HashRouter } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  RouterProvider, createHashRouter
+} from 'react-router-dom';
+import './index.css';
+import App from './App';
+
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
